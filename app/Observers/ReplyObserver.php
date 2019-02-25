57aa77e65212e3c3dee75753433a91e3af50b9cf
GITDengMing <13767267543@163.com>
2019-02-25 07:06:56 +0000

    用户发表评论

---
+++ app/Observers/ReplyObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Reply;
+
+
+
+// creating, created, updating, updated, saving,
+    // saved,  deleting, deleted, restoring, restored
+class ReplyObserver
+{
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count= $reply->topic->replies->count();
+
+        $reply->topic->save();
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
 	{
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
         //Carbon时间日期本地化
         Carbon::setLocale('zh');
     }
